754134c8d874a7d3c24b8424807887572c968e77
Luizeduardovae <duduvivas18@hotmail.com>
2023-05-15 01:47:46 -0300

    Done

--- DioChallange.playground/Contents.swift
+++ DioChallange.playground/Contents.swift
@@ -1,3 +1,15 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let nome = "Steve"
+var sobrenome: String? =  nil
+
+let sobrenome2 =  sobrenome ?? "Wozniak"
+
+print("Nome do criador da Apple é \(nome) \(sobrenome2) ")
+
+
+if let sobrenome2 = sobrenome {
+    print("O nome do criador da Apple é \(nome) \(sobrenome2)")
+}   else {
+    print("O nome dele é \(nome) \(sobrenome2)")
+}
